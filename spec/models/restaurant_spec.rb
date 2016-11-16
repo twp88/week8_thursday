731f38b4e9c11b2058780ad25144d569f7753a05
Rory Aitchison <rory.aitchison@gmail.com>
2016-11-16 17:41:01 +0000

    mvp

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 describe Restaurant, type: :model do
+
   it 'is not vaid with a name of less than three characters' do
     restaurant = Restaurant.new(name: "kf")
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it "is not valid unless it has a unique name" do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
 end
